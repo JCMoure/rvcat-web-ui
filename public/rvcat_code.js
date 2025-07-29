dfe1e02c46eed391738328368c6e3d404b8dab71
JCMoure <juancarlos.moure@uab.es>
2025-07-29 17:15:35 +0200

    CODE

--- public/rvcat_code.js
+++ public/rvcat_code.js
@@ -8,22 +8,22 @@ rvcat._processor.list_processors_json()
 
 // SHOW program
 const PROG_SHOW              = 'str(rvcat._program)'
-const PROG_SHOW_DEPENDENCIES = `rvcat._program.show_dependencies()`
-const PROG_SHOW_DEPENDENCIES_GRAPHVIZ = `rvcat._program.get_dependencies_grapviz(num_iters=3)`
+const PROG_SHOW_DEPENDENCIES = `rvcat._program.show_performance_analysis()`
+const PROG_SHOW_DEPENDENCIES_GRAPHVIZ = `rvcat._program.show_graphviz()`
 const RUN_PROGRAM_TIMELINE = 'rvcat._scheduler.format_timeline()'
 const RUN_PROGRAM_ANALYSIS = 'rvcat._scheduler.format_analysis_json()'
 const RUN_PROGRAM_MEMTRACE = 'rvcat._scheduler.format_memtrace()'
 
 
 function prog_show_dependencies_graphviz(num_iters) {
-    return `rvcat._program.get_dependencies_grapviz(num_iters=${num_iters})`
+    return `rvcat._program.show_graphviz()`
 }
 
-const PROG_SHOW_CRITICAL_PATHS_GRAPHVIZ = `rvcat._program.get_recurrent_paths_graphviz()`
-const PROG_SHOW_ANNOTATED    = `rvcat._program.annotate_action()`
-const PROG_SHOW_EXECUTION    = `rvcat._program.annotate_execution()`
+const PROG_SHOW_CRITICAL_PATHS_GRAPHVIZ = `rvcat._program.show_graphviz()`
+const PROG_SHOW_ANNOTATED    = `rvcat._program.show_code()`
+const PROG_SHOW_EXECUTION    = `rvcat._program.show_code()`
 const PROG_SHOW_MEMORY       = `rvcat._program.show_memory_trace()`
-const PROG_SHOW_STATIC_PERFORMANCE = `rvcat._program.show_static_performance_analysis()`
+const PROG_SHOW_STATIC_PERFORMANCE = `rvcat._program.show_performance_analysis()`
 
 const SHOW_TIMELINE = 'rvcat._scheduler.format_timeline()'
 
@@ -36,12 +36,9 @@ const SHOW_PROCESSOR = 'rvcat._processor.json()'
 
 const GET_PROGRAM_JSON = 'rvcat._program.json()'
 
-// SHOW isa
-const SHOW_ISA = 'str(rvcat._isa)'
-
 // RUN program
 const RUN_PROGRAM_PREAMBLE = function() {
-    let res = `rvcat._scheduler.load_program(rvcat._program, iterations=${currentIterations()}, window_size=${currentROBSize()})\n`
+    let res = `rvcat._scheduler.init_scheduler(iterations=${currentIterations()}, window_size=${currentROBSize()})\n`
     return res;
 }
 
