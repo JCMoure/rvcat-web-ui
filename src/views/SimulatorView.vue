686760b0fbbc5bcb3e99a35345fcbf8a09bf138e
JCMoure <juancarlos.moure@uab.es>
2026-05-18 00:46:54 +0200

    fix memory size computation

--- src/views/SimulatorView.vue
+++ src/views/SimulatorView.vue
@@ -25,7 +25,7 @@ const { importRVCAT }               = useRVCAT_Api();
   const STORAGE_KEY = 'rvcatOptions'
 
   const rvcatOptions = {
-    version: "1.06",
+    version: "1.07",
     year:    2026
   }
 
